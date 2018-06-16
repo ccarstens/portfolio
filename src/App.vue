5604aca9ba467e6a9c894ffa0d1e3170dd902de1
Cornelius Carstens <cornelius@awesome-boys.com>
2018-06-16 16:45:41 +0200

    testing video handling

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <VideoItem :play="play" :src="src"></VideoItem>
+
+      <button @click="changeValue">Test</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import VideoItem from './components/VideoItem'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    data () {
+        return {
+            play: true,
+            src: '../assets/scrollcast_1.mp4'
+        }
+    },
+    methods: {
+        changeValue () {
+            this.play = !this.play
+        }
+    },
+    components: {
+        VideoItem
+    }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
--- src/components/VideoItem.vue
+++ src/components/VideoItem.vue
@@ -1,11 +1,46 @@
 <template>
-    
+    <video v-on:load="init" :src="srcUrl"></video>
+
 </template>
 
 <script>
-  export default {
-    name: "VideoItem"
-  }
+export default {
+    name: 'VideoItem',
+    props: [
+        'play',
+        'src'
+    ],
+    data () {
+        return {
+            srcUrl: ''
+        }
+    },
+    created () {
+        console.log(this.$props.src)
+        this.srcUrl = this.$props.src
+    },
+    mounted () {
+        this.updatePlay(this.$props.play)
+        console.log(this.$el)
+    },
+    methods: {
+        init () {
+            this.$el.play()
+        },
+        updatePlay (play) {
+            if(play) this.$el.play()
+            else this.$el.pause()
+        },
+        changeSource(){
+            this.srcUrl = '../assets/scrollcast_1.mp4'
+        }
+    },
+    watch: {
+        play (newPlayState, b) {
+            this.updatePlay(newPlayState)
+        }
+    }
+}
 </script>
 
 <style scoped>
