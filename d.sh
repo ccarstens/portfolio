47f10183cc92c50619ab5dca3046919902f8d8b3
Cornelius Carstens <cornelius@awesome-boys.com>
2018-06-17 19:33:33 +0200

    adding deployment scripts

---
+++ d.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+
+ssh -i ~/.ssh/id_rsa_portfolio hnwirq9s4io4@160.153.131.203 bash -s < deploy.sh
---
+++ src/components/HeaderElement.vue
@@ -0,0 +1,15 @@
+<template>
+    <header id="site-header">
+        <nav></nav>
+    </header>
+</template>
+
+<script>
+    export default {
+        name: "HeaderElement"
+    }
+</script>
+
+<style scoped>
+
+</style>
